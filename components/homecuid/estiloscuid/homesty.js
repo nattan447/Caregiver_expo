af1ac72f6c061bee2d65b7e8dbbd6cb29426f820
nattan447 <nattanferreira58@gmail.com>
2023-12-14 15:14:34 -0300

    home em processo

---
+++ components/homecuid/estiloscuid/homesty.js
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default StyleSheet.create({
+  header: {
+    marginTop: "20%",
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headertxt: {
+    color: "#C77B43",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  main: {
+    marginTop: "7%",
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+  },
+  mainusername: {
+    fontSize: 24,
+    color: "#C77B43",
+  },
+  btnwicons: {
+    marginTop: "8%",
+    flexDirection: "row",
+    backgroundColor: "#F1EBEB",
+    width: 250,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 7,
+  },
+  btnwconstxt: {
+    fontSize: 17,
+    fontWeight: "bold",
+    color: "#C77B43",
+    right: "20%",
+  },
+});
--- components/homecuid/homecuid.tsx
+++ components/homecuid/homecuid.tsx
@@ -16,14 +16,47 @@ import { RootStackParamList } from "../../App";
 import { Btn } from "../../desginscomponents/authenticheadrs";
 import homeloginscss from "../../estilos/homeloginscss";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-
+import homesty from "./estiloscuid/homesty";
 import React from "react";
 
 const Homecuid = () => {
   return (
-    <View style={homeloginscss.container}>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={homeloginscss.container}>
+      <View style={homesty.header}>
+        <Text style={homesty.headertxt}>Caregiver</Text>
+        <TouchableOpacity>
+          <Image
+            source={require("../../assets/gear.png")}
+            style={{
+              left: "150%",
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={homesty.main}>
+        <Image
+          source={require("../../assets/user.png")}
+          style={{ height: 90, width: 90 }}
+        />
+        <Text style={homesty.mainusername}>Nattan ferreira</Text>
+
+        <TouchableOpacity style={homesty.btnwicons}>
+          <Image
+            source={require("../../assets/notifications.png")}
+            style={{ height: 32, width: 32, right: "100%" }}
+          />
+          <Text style={homesty.btnwconstxt}>notificações</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={homesty.btnwicons}>
+          <Image
+            source={require("../../assets/add.png")}
+            style={{ height: 27, width: 27, right: "100%" }}
+          />
+          <Text style={homesty.btnwconstxt}>Dependente</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 export default Homecuid;
